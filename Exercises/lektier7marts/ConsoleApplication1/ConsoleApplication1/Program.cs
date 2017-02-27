267913ef1b5c944233ee0ba66a512409050a68e3
Stine Bagger Holt <sbh@tm-group.com>
2017-02-27 14:44:14 +0100

    første opgave til 7 marts

---
+++ Exercises/lektier7marts/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til Calculatron ;)");
+            Calculatron();
+        }
+
+        static void Calculatron()
+        {
+            GreetUser();
+            int MitValg = GetInt();
+
+            if (MitValg == 0)
+            {
+                return;
+            }
+            else if (MitValg == 1)
+            {
+                AddNumbers();
+            }
+            else if (MitValg == 2)
+            {
+                Substraktion();
+            }
+            else if (MitValg == 3)
+            {
+                Multiplikation();
+            }
+            else if (MitValg == 4)
+            {
+                Division();
+            }
+
+            Console.WriteLine("");
+            Calculatron();
+        }
+
+        static int GetInt()
+        {
+            Console.Write("indtast tal: ");
+            int input = Int32.Parse(Console.ReadLine());
+            return input;
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("Hvad skal jeg gøre for dig?");
+            Console.WriteLine("Tast 0 for at lukke programmet");
+            Console.WriteLine("Tast 1 for addition");
+            Console.WriteLine("Tast 2 for substraktion");
+            Console.WriteLine("Tast 3 for multiplikation");
+            Console.WriteLine("Tast 4 for divistion");
+        }
+
+        static void AddNumbers()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Det bare skønt, giv mig de tal du vil ligge sammen: ");
+            int firstNumber = GetInt();
+            int secondNumber = GetInt();
+            int Sum = firstNumber + secondNumber;
+            Console.WriteLine("Okay, lad os se "+firstNumber+ " + " + secondNumber+ " = " + Sum);
+        }
+
+        static void Substraktion()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Jamen så det da det vi gør, giv mig de tal du vil trække fra hinanden sammen: ");
+            int firstNumber = GetInt();
+            int secondNumber = GetInt();
+            int Sum = firstNumber - secondNumber;
+            Console.WriteLine("Okay, lad os se " + firstNumber + " - " + secondNumber + " = " + Sum);
+        }
+
+        static void Multiplikation()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Jamen så det da det vi gør, giv mig de tal du vil gange sammen: ");
+            int firstNumber = GetInt();
+            int secondNumber = GetInt();
+            int Sum = firstNumber * secondNumber;
+            Console.WriteLine("Okay, lad os se " + firstNumber + " * " + secondNumber + " = " + Sum);
+        }
+
+        static void Division()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Jamen så det da det vi gør, giv mig de tal du vil dividere med hinanden: ");
+            int firstNumber = GetInt();
+            int secondNumber = GetInt();
+            int Sum = firstNumber / secondNumber;
+            Console.WriteLine("Okay, lad os se " + firstNumber + " / " + secondNumber + " = " + Sum);
+        }
+    }
+}
---
+++ Exercises/lektier7marts/Lektier7marts/Lektier7marts/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektier7marts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHelloWorld();
+            Console.WriteLine(Biggerthan10(9));
+
+            Console.Write("What is your name? ");
+            CurrentUser(Console.ReadLine());
+
+            Console.Write("Skriv dit første tal: ");
+            int firstTal = Int32.Parse(Console.ReadLine());
+            Console.Write("Skriv dit andet tal: ");
+            int secondTal = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tilsammen er det: " + Plus2Tal(firstTal, secondTal));
+
+            metode1();
+            Console.WriteLine("metode1 sluttet og nu tilbage i main metode");
+
+            Console.ReadKey();
+        }
+
+        static void MyHelloWorld()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static Boolean Biggerthan10(int heltal)
+        {
+            if (heltal > 10)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+                
+        }
+
+        static string CurrentUser(string navnet)
+        {
+            Console.WriteLine("Hallo " + navnet + ", nice to meet you!");
+            return navnet;
+        }
+
+        static int Plus2Tal(int first, int second)
+        {
+            int sum = first + second;
+            return sum;
+        }
+
+        static void metode1()
+        {
+            Console.WriteLine("metode1 begyndt .. ");
+            metode2();
+            Console.WriteLine("metode2 sluttet og nu tilbage i metode 1");
+        }
+        static void metode2()
+        {
+            Console.WriteLine("metode2 begyndt .. ");
+            metode3();
+            Console.WriteLine("metode3 slut og tilbage i metode2 ..  ");
+        }
+        static void metode3()
+        {
+            Console.WriteLine("metode3 begyndt .. ");
+            CurrentUser("metode3");
+        }
+    }
+}
