62637f217ed9c6f8639aa1ea078ace580c65bc6e
Stine Bagger Holt <sbh@tm-group.com>
2017-03-24 12:59:42 +0100

    nye versioner af calculators på forskellige måder

---
+++ Exercises/Calc program/ConsoleApplication2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator();
+            
+        }
+
+        static void Calculator()
+        {
+            Console.WriteLine("Hvad skal vi regne?");
+            Console.Write("Enter value no 1. ");
+            int value1 = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter value no 2. ");
+            int value2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vælg hvad du vil: (A)dd, (S)ubstract, (M)ultiply, (D)ivide");
+            Console.Write("Dit valg (skriv med stort): ");
+            string valgt = Console.ReadLine();
+
+            switch (valgt)
+            {
+                case "A":
+                    Console.WriteLine("Resultatet er: " + AddNumbers(value1, value2));
+                    break;
+                case "S":
+                    Console.WriteLine("Resultatet er: " + SubtractNumbers(value1, value2));
+                    break;
+                case "M":
+                    Console.WriteLine("Resultatet er: " + MultipleNumbers(value1, value2));
+                    break;
+                case "D":
+                    Console.WriteLine("Resultatet er: " + DivideNumbers(value1, value2));
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine();
+            Calculator();
+        }
+
+        static int AddNumbers(int tal1, int tal2)
+        {
+            return tal1 + tal2;
+        }
+        static int SubtractNumbers(int tal1, int tal2)
+        {
+            return tal1 - tal2;
+        }
+        static int MultipleNumbers(int tal1, int tal2)
+        {
+            return tal1 * tal2;
+        }
+        static int DivideNumbers(int tal1, int tal2)
+        {
+            return tal1 / tal2;
+        }
+    }
+}
---
+++ Exercises/anden form for calc/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til Calculatron");
+            Calc();
+        }
+
+        static void Calc()
+        {
+            GreetUser();
+            int MitValg = GetInt();
+
+            switch (MitValg)
+            {
+                case 0:
+                    break;
+                case 1:
+                    Console.WriteLine("Du valgte at lægge sammen");
+                    int sum = AddNumbers(GetInt(), GetInt());
+                    Console.WriteLine("Det bliver: " + sum);
+                    Console.WriteLine("");
+                    Calc();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static int GetInt()
+        {
+                Console.Write("indtast tal: ");
+                int input = Int32.Parse(Console.ReadLine());
+                return input;
+        }
+
+
+        static void GreetUser()
+        {
+            Console.WriteLine("Hvad skal jeg gøre for dig?");
+            Console.WriteLine("Tast 0 for at lukke programmet");
+            Console.WriteLine("Tast 1 for addition");
+            Console.WriteLine("Tast 2 for substraktion");
+            Console.WriteLine("Tast 3 for multiplikation");
+            Console.WriteLine("Tast 4 for divistion");
+        }
+
+        static int AddNumbers(int valg1,int valg2)
+        {
+            return valg1 + valg2;
+        }
+    }
+}
